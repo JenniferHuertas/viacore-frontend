ae33119991f288f75af5548d7b003897059db1b6
TFColmenares <131204344+TFColmenares@users.noreply.github.com>
2026-05-26 22:43:50 -0500

    Update socket.ts

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,15 +1,11 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL =
-  process.env.NEXT_PUBLIC_API_URL;
+const SOCKET_URL = process.env.NEXT_PUBLIC_API_URL;
 
-export const socket = io(
-  SOCKET_URL!,
-  {
-    autoConnect: false,
+export const socket = io(SOCKET_URL!, {
+  autoConnect: false,
 
-    transports: ["websocket"],
+  transports: ["websocket", "polling"],
 
-    withCredentials: true,
-  },
-);
+  withCredentials: true,
+});
