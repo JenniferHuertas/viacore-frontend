817133e3ecdb13a62dc6c7fb162f04187aed171d
Axel Aranda <axelemilianoaranda1993@gmail.com>
2026-05-27 17:35:11 -0300

    mejora de schema de validaciones de form de agenda

--- src/validations/meeting.validations.ts
+++ src/validations/meeting.validations.ts
@@ -1,22 +1,30 @@
 import { z } from "zod";
 
 const esDiaHabil = (dateStr: string) => {
-  const date = new Date(dateStr);
-  const day = date.getDay(); 
-  return day >= 1 && day <= 5;
+  const [year, month, day] = dateStr.split("-").map(Number);
+
+  const date = new Date(year, month - 1, day);
+
+  const weekDay = date.getDay();
+
+  return weekDay >= 1 && weekDay <= 5;
 };
 
 const esHorarioLaboral = (time: string) => {
   const [h, m] = time.split(":").map(Number);
+
   const minutos = h * 60 + m;
 
   return minutos >= 9 * 60 && minutos <= 16 * 60 + 30;
 };
 
 export const meetingSchema = z.object({
-  fecha: z.string().refine(esDiaHabil, "Solo días hábiles (Lun–Vie)"),
+  fecha: z
+    .string()
+    .refine(esDiaHabil, "Solo días hábiles (Lun–Vie)"),
 
   horario: z
     .string()
     .refine(esHorarioLaboral, "Horario permitido: 09:00 - 16:30"),
 });
+
