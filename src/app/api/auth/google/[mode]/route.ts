fbff61c796066fd633044609cb9377a827b1bf12
TFColmenares <131204344+TFColmenares@users.noreply.github.com>
2026-05-27 23:30:03 -0500

    Update route.ts

--- src/app/api/auth/google/[mode]/route.ts
+++ src/app/api/auth/google/[mode]/route.ts
@@ -1,20 +1,11 @@
+// src/app/api/auth/google/[mode]/route.ts
 import { NextResponse } from "next/server";
 
 export async function GET(
   request: Request,
-  {
-    params,
-  }: {
-    params: {
-      mode: string;
-    };
-  },
+  { params }: { params: Promise<{ mode: string }> }
 ) {
-
-  const backendUrl =
-    `${process.env.NEXT_PUBLIC_API_URL}/auth/google/${params.mode}`;
-
-  return NextResponse.redirect(
-    backendUrl,
-  );
+  const { mode } = await params;
+  const backendUrl = `${process.env.NEXT_PUBLIC_API_URL}/auth/google/${mode}`;
+  return NextResponse.redirect(backendUrl);
 }
